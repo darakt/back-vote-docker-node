5fc965194ecc5a087e8f92e1c24ef094d684177e
darakt <kheriboum@gmail.com>
2022-04-12 09:57:40 +0200

    :white_check_mark: Jest is now working

--- vote/voteController.test.js
+++ vote/voteController.test.js
@@ -1,4 +1,10 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import userController from "./voteController.js.js";
+import voteController from "./voteController.js";
 import pool from "../config.js";
+
+describe('voteController ', () => {
+    test('should give two random characters', () => {
+        expect(0).toBe(0);
+    });
+});
